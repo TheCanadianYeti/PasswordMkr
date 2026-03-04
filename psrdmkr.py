3f162a92a5ca4a25485c36d17b503c215d6a870a
CanadianYeti <Marcus.podnar@gmail.com>
2026-03-04 12:51:13 -0500

    Create psrdmkr.py

---
+++ psrdmkr.py
@@ -0,0 +1,36 @@
+import random as rdm
+
+password = ""
+characters = [
+    list("abcdefghijklmnopqrstuvwxyz"),
+    list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+    list("1234567890"),
+    list("!@#$%^&*()[]{}-_+=~")
+]
+
+# Get password length
+while True:
+    length = input("How many characters long?: ")
+    if length.isnumeric():
+        length = int(length)
+        break
+    else:
+        print("You cannot enter anything other than a whole number!")
+
+# Special character option
+special = input("Are special characters allowed (y/n)?: ").lower()
+if special in ["y", "yes"]:
+    print("Ok, special characters added.")
+    addSpecial = 1
+else:
+    print("Ok, no special characters added.")
+    addSpecial = 0
+
+# Generate password
+for i in range(length):
+    charType = rdm.randint(0, 2 + addSpecial)  # include specials if allowed
+    amountOfChar = len(characters[charType])
+    charPosition = rdm.randint(0, amountOfChar - 1)
+    password += characters[charType][charPosition]
+
+print("Your password is:", password)
